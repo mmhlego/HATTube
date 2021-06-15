32824c4fa6e7338e5036a7c82e3a202759e75c0a
Pouyaafraz123 <pouyaafraz@gmail.com>
2021-06-15 14:39:12 +0430

    Sign Up Finished

---
+++ src/common/controller/SignUP2.java
@@ -0,0 +1,46 @@
+package common.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static model.StyleController.ChangeTextField;
+import static model.StyleController.LabelFloat;
+
+public class SignUP2 implements Initializable {
+
+    @FXML
+    private TextField PhoneNumberTXF;
+
+    @FXML
+    private Label PhoneNumberLBL;
+
+    @FXML
+    private TextField EmailTXF;
+
+    @FXML
+    private Label EmailLBL;
+
+    @FXML
+    private JFXButton NextBTN;
+
+    @FXML
+    private ImageView PhoneNumberIMG;
+
+    @FXML
+    private ImageView EmailIMG;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        LabelFloat(PhoneNumberTXF, PhoneNumberLBL, PhoneNumberIMG);
+        LabelFloat(EmailTXF, EmailLBL, EmailIMG);
+        NextBTN.setOnMouseEntered(e -> ChangeTextField(NextBTN, NextBTN.getWidth() * 1.2));
+        NextBTN.setOnMouseExited(e -> ChangeTextField(NextBTN, NextBTN.getWidth() * 10 / 12));
+    }
+}
