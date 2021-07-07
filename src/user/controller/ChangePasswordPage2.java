04e5507d0da3304b3ee20ded8fbecaf00acdfe12
Pouya <77690159+pouyaafraz123@users.noreply.github.com>
2021-07-07 20:42:02 +0430

    All Change Password Pages Finished

---
+++ src/user/controller/ChangePasswordPage2.java
@@ -0,0 +1,25 @@
+package user.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class ChangePasswordPage2 {
+
+    @FXML
+    private AnchorPane Page;
+
+    @FXML
+    private ImageView CancelBTN;
+
+    @FXML
+    private JFXTextField CurrentPasswordTXF;
+
+    @FXML
+    private ImageView NextBTN;
+
+    @FXML
+    private ImageView BackBTN;
+
+}
---
+++ src/user/controller/ChangePasswordPage4.java
@@ -0,0 +1,25 @@
+package user.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class ChangePasswordPage4 {
+
+    @FXML
+    private AnchorPane Page;
+
+    @FXML
+    private ImageView CancelBTN;
+
+    @FXML
+    private JFXTextField RepeatPasswordTXF;
+
+    @FXML
+    private ImageView ChangePasswordBTN;
+
+    @FXML
+    private ImageView BackBTN;
+
+}
