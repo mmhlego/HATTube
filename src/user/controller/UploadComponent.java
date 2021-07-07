b3840927d821b009cd2cb62d14b76835e6faaeb1
Pouya <77690159+pouyaafraz123@users.noreply.github.com>
2021-07-07 21:42:38 +0430

    Upload component finished

---
+++ src/user/controller/UploadComponent.java
@@ -0,0 +1,22 @@
+package user.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class UploadComponent {
+
+    @FXML
+    private AnchorPane Page;
+
+    @FXML
+    private ImageView CancelBTN;
+
+    @FXML
+    private Button EnterUrlBTN;
+
+    @FXML
+    private Button SelectBTN;
+
+}
