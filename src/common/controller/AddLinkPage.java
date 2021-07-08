0d53d4a425ec9d81533985e2afc797e898fc03fa
Pouya <77690159+pouyaafraz123@users.noreply.github.com>
2021-07-08 13:52:07 +0430

    Add Component And Add link component and Add link Page finished

---
+++ src/common/controller/AddLinkPage.java
@@ -0,0 +1,18 @@
+package common.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class AddLinkPage {
+
+    @FXML
+    private VBox ComponentPlaceBOX;
+
+    @FXML
+    private Button AddBTN;
+
+    @FXML
+    private Button SaveBTN;
+
+}
---
+++ src/common/controller/component/AddComponent.java
@@ -0,0 +1,11 @@
+package common.controller.component;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class AddComponent {
+
+    @FXML
+    private AnchorPane AddLinkANC;
+
+}
