d3fd01a2427090d320b9f1db76e2a7d5034612f4
Pouya <77690159+pouyaafraz123@users.noreply.github.com>
2021-07-10 00:22:53 +0430

    User Search Page and User Component Finished

---
+++ src/user/controller/UserComponent.java
@@ -0,0 +1,31 @@
+package user.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class UserComponent {
+
+    @FXML
+    private ImageView AvatarIMG;
+
+    @FXML
+    private Label IdLBL;
+
+    @FXML
+    private Label UsernameLBL;
+
+    @FXML
+    private Button EditUserBTN;
+
+    @FXML
+    private Button EditAccessBTN;
+
+    @FXML
+    private Label PremiumLBL;
+
+    @FXML
+    private Label AdiminLBL;
+
+}
---
+++ src/user/controller/UserSearchPage.java
@@ -0,0 +1,26 @@
+package user.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class UserSearchPage {
+
+    @FXML
+    private AnchorPane LoginAnchor;
+
+    @FXML
+    private ImageView CloseBTN;
+
+    @FXML
+    private TextField SearchbarTXF;
+
+    @FXML
+    private ImageView SearchIMG;
+
+    @FXML
+    private VBox ComponentPlace;
+
+}
