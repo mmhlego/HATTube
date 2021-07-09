0631b75b9c2b7851187ca59473a75319a04d2891
Pouya <77690159+pouyaafraz123@users.noreply.github.com>
2021-07-09 14:56:41 +0430

    Medium Channel Component And Not Found Page

---
+++ src/common/controller/component/NotFoundPage.java
@@ -0,0 +1,11 @@
+package common.controller.component;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class NotFoundPage {
+
+    @FXML
+    private Label NFL;
+
+}
---
+++ src/user/controller/MediumChannelComponent.java
@@ -0,0 +1,25 @@
+package user.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class MediumChannelComponent {
+
+    @FXML
+    private ImageView AvatarIMG;
+
+    @FXML
+    private Label ChannelCreatorNameLBL;
+
+    @FXML
+    private Label ChannelNameLBL;
+
+    @FXML
+    private Button FollowBTN;
+
+    @FXML
+    private Label ChannelCreatorNameLBL1;
+
+}
