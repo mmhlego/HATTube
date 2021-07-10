b204611be23b5936bd91811ed8acad35801af9c5
Pouya <77690159+pouyaafraz123@users.noreply.github.com>
2021-07-10 12:13:38 +0430

    logo color changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.io.File;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,6 +5,8 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import java.io.File;
 
 public class Main extends Application {
     public static void main(String[] args) throws Exception {
@@ -18,8 +18,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // FXMLLoader loader = new FXMLLoader(new File("src/common/visual/StartPage.fxml").toURI().toURL());
-        FXMLLoader loader = new FXMLLoader(new File("src/common/visual/ContentPage.fxml").toURI().toURL());
+        FXMLLoader loader = new FXMLLoader(new File("src/common/visual/StartPage.fxml").toURI().toURL());
+        /*   FXMLLoader loader = new FXMLLoader(new File("src/common/visual/ContentPage.fxml").toURI().toURL());*/
         Parent parent = loader.load();
         Scene scene = new Scene(parent);
         scene.setFill(Color.TRANSPARENT);
